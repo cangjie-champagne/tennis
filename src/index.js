f80bf6877061e280fd1b59e4baf64464431113ec
João Pedro <joaopjt@icloud.com>
2023-09-14 23:10:19 -0300

    added u() method values

--- src/index.js
+++ src/index.js
@@ -892,7 +892,7 @@ class Tent {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['bow'],
       web: []
     };
     
@@ -908,19 +908,19 @@ class Tent {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['one'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['day'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['middle'];
           break;
       }
     }
