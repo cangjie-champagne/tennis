76fbd164143e14960983023a20b9fb6cd5a2463c
João Pedro <joaopjt@icloud.com>
2023-09-14 23:07:46 -0300

    added q() method values

--- src/index.js
+++ src/index.js
@@ -732,7 +732,7 @@ class Tent {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['bamboo'],
       web: []
     };
     
@@ -748,19 +748,19 @@ class Tent {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['predict'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['ten'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['moon'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['one'];
           break;
       }
     }
