1d4e218afc8a0c2660a58d4c07dc6d08e1342d39
João Pedro <joaopjt@icloud.com>
2023-09-14 23:10:53 -0300

    added v() method values

--- src/index.js
+++ src/index.js
@@ -932,7 +932,7 @@ class Tent {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['female'],
       web: []
     };
     
@@ -948,19 +948,19 @@ class Tent {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['ten'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['disaster'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['mouth'];
           break;
       }
     }
